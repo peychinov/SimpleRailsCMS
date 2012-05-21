a458ccbd477e092e0fe38fffe5137725fa5e8c64
Dimitar Peychinov <dp86jd@gmail.com>
2012-05-21 16:51:40 +0300

    Added category parenting functionality (not yet showing nicely).

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
   attr_accessible :parent_id, :title
 
   has_many :pages
+  
+  has_many  :child_categories, :class_name => "Category"
+  belongs_to :parent_category, :class_name => "Category", :foreign_key => "parent_id"
 end
