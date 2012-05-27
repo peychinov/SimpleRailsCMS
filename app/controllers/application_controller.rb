a1187f9619c31d785311b5fcc8f778a2b1a0a6d3
Dimitar Peychinov <dp86jd@gmail.com>
2012-05-27 21:37:11 +0300

    Added a site_bar on the top of the page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def track_last_public_url
+  	@last_public_url = request.url
+  	@is_in_editor = false
+  end
+
+  def track_last_admin_url
+  	@last_admin_url = request.url
+  	@is_in_editor = true
+  end
 end
