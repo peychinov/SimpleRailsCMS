116297c4c396c7b93709043c2c13f42af98d216b
Dimitar Peychinov <dp86jd@gmail.com>
2013-04-30 21:30:16 +0300

    Removed the show method from the admin categories controller.

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -11,17 +11,6 @@ class Admin::ArticlesController < Admin::AdminController
     end
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
-  def show
-    @article = Article.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @article }
-    end
-  end
-
   # GET /articles/new
   # GET /articles/new.json
   def new
