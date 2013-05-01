1e49c7e9beee8b211aeb1adcc5e3d48cdcbc9893
Dimitar Peychinov <dp86jd@gmail.com>
2013-05-01 22:05:11 +0300

    Improved the navigation.

--- app/assets/javascripts/common.coffee
+++ app/assets/javascripts/common.coffee
@@ -1,5 +1,9 @@
 $ ->
 
+  $('.nav a').each ->
+    if $(@).attr('href') == $(location).attr('pathname')
+      $(@).parent().addClass("active")
+
   Tipped.create('.tipped')
 
   
--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,6 +1,11 @@
 (function() {
 
   $(function() {
+    $('.nav a').each(function() {
+      if ($(this).attr('href') === $(location).attr('pathname')) {
+        return $(this).parent().addClass("active");
+      }
+    });
     return Tipped.create('.tipped');
   });
 
