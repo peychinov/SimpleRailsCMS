8a78eda3db1627517311a900610824ec8fbbc28c
Dimitar Peychinov <dp86jd@gmail.com>
2013-05-08 19:55:16 +0300

    Two little fixes.

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -7,7 +7,7 @@ class VersionsController < ApplicationController
       @version.item.destroy
     end
 
-    action = params[:redo] == 'true' ? "Undo" : "Redo"
+    action = params[:redo] == 'true' ? "undo" : "redo"
     link = view_context.link_to(view_context.image_tag("#{action}.png"), revert_version_path(@version.next, :redo => !params[:redo]), :method => :post, title: action, class: 'tipped')
 
     redirect_to :back, :notice => "Undid #{@version.event}. #{link}"
