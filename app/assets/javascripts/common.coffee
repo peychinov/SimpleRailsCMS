8a84111cae9c3da00f830b121824eef0d382d2bd
Dimitar Peychinov <dp86jd@gmail.com>
2013-05-22 15:09:41 +0300

    Improvements on the jstree.

--- app/assets/javascripts/common.coffee
+++ app/assets/javascripts/common.coffee
@@ -8,10 +8,53 @@ $ ->
 
   Tipped.create('.tipped')
 
-  $("#admin_categories_jstree").jstree
+  bg = window.location.href.indexOf("/bg/") > -1
+
+  $("#admin_categories_jstree").jstree(
     json_data:
       ajax:
         url: "/admin/categories.json"
         data: (n) ->
           id: (if n.attr then n.attr("id") else '')
-    plugins: ["themes", "json_data", "ui", "crrm", "contextmenu"]
+    contextmenu:
+      items:
+        create:
+          label: if bg then 'Създай' else 'Create'
+        rename:
+          label: if bg then 'Преименувай' else 'Rename'
+        remove:
+          label: if bg then 'Премахни' else 'Remove'
+        ccp: false
+    plugins: ["themes", "json_data", 'ui', 'crrm', 'contextmenu']
+  ).bind("loaded.jstree", (event, data) ->
+    $(this).jstree("open_all")
+  ).bind("select_node.jstree", (evt, data) ->
+    window.location = '/admin/categories/' + data.rslt.obj.attr("id") + '/edit'
+  ).bind("create.jstree", (evt, data) ->
+    parent_id = data.inst._get_parent(data.rslt.obj).attr("id")
+    title = data.rslt.obj[0].innerText.replace /^\s+/g, ""
+    $.ajax '/admin/categories.json',
+      type: 'POST'
+      dataType: 'json'
+      data:
+        category:
+          parent_id: parent_id
+          title: title
+      complete:(response) =>
+        new_id = JSON.parse(response.responseText).id
+        data.rslt.obj.attr("id", new_id)
+  ).bind("rename_node.jstree", (evt, data) ->
+    id = data.rslt.obj.attr("id")
+    unless (typeof id == "undefined")
+      title = data.rslt.obj[0].innerText.replace /^\s+/g, ""
+      $.ajax "/admin/categories/#{id}.json",
+        type: 'PUT'
+        dataType: 'json'
+        data:
+          category:
+            title: title
+  ).bind("delete_node.jstree", (evt, data) ->
+    id = data.rslt.obj.attr("id")
+    $.ajax "/admin/categories/#{id}.json",
+      type: 'DELETE'
+  )
--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,5 +1,7 @@
 (function() {
   $(function() {
+    var bg;
+
     $('.best_in_place').best_in_place();
     $('.nav a').each(function() {
       if ($(this).attr('href') === $(location).attr('pathname')) {
@@ -7,6 +9,7 @@
       }
     });
     Tipped.create('.tipped');
+    bg = window.location.href.indexOf("/bg/") > -1;
     return $("#admin_categories_jstree").jstree({
       json_data: {
         ajax: {
@@ -18,7 +21,70 @@
           }
         }
       },
-      plugins: ["themes", "json_data", "ui", "crrm", "contextmenu"]
+      contextmenu: {
+        items: {
+          create: {
+            label: bg ? 'Създай' : 'Create'
+          },
+          rename: {
+            label: bg ? 'Преименувай' : 'Rename'
+          },
+          remove: {
+            label: bg ? 'Премахни' : 'Remove'
+          },
+          ccp: false
+        }
+      },
+      plugins: ["themes", "json_data", 'ui', 'crrm', 'contextmenu']
+    }).bind("loaded.jstree", function(event, data) {
+      return $(this).jstree("open_all");
+    }).bind("select_node.jstree", function(evt, data) {
+      return window.location = '/admin/categories/' + data.rslt.obj.attr("id") + '/edit';
+    }).bind("create.jstree", function(evt, data) {
+      var parent_id, title,
+        _this = this;
+
+      parent_id = data.inst._get_parent(data.rslt.obj).attr("id");
+      title = data.rslt.obj[0].innerText.replace(/^\s+/g, "");
+      return $.ajax('/admin/categories.json', {
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          category: {
+            parent_id: parent_id,
+            title: title
+          }
+        },
+        complete: function(response) {
+          var new_id;
+
+          new_id = JSON.parse(response.responseText).id;
+          return data.rslt.obj.attr("id", new_id);
+        }
+      });
+    }).bind("rename_node.jstree", function(evt, data) {
+      var id, title;
+
+      id = data.rslt.obj.attr("id");
+      if (!(typeof id === "undefined")) {
+        title = data.rslt.obj[0].innerText.replace(/^\s+/g, "");
+        return $.ajax("/admin/categories/" + id + ".json", {
+          type: 'PUT',
+          dataType: 'json',
+          data: {
+            category: {
+              title: title
+            }
+          }
+        });
+      }
+    }).bind("delete_node.jstree", function(evt, data) {
+      var id;
+
+      id = data.rslt.obj.attr("id");
+      return $.ajax("/admin/categories/" + id + ".json", {
+        type: 'DELETE'
+      });
     });
   });
 
