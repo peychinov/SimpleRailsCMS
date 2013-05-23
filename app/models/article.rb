ea64931bd7b520ad7170eafecb08cd8c165dff58
Dimitar Peychinov <dp86jd@gmail.com>
2013-05-23 14:40:52 +0300

    Fixes on the search, add-ons for the tags and other small changes.

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,11 +17,31 @@ class Article < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
 
+  mapping do
+    indexes :category_id, type: 'integer'
+    indexes :tag_names
+    indexes :title, boost: 10
+    indexes :content
+  end
+
   def self.search(params)
     tire.search(load: true, per_page: 3, page: params[:page] || 1) do
-      query { string params[:query]} if params[:query].present?
-      sort { by :title, 'desc' }
+      query do
+        boolean do
+          must { string params[:keywords], default_operator: "AND" } if params[:keywords].present?
+          must { term :category_id, params[:category_id] } if params[:category_id].present?
+          must { term :tag_names, params[:tag] } if params[:tag].present?
+        end
+      end
+      sort { by :title, 'desc' } if params[:keywords].blank?
     end
   end
 
+  def to_indexed_json
+    to_json(methods: [:tag_names])
+  end
+
+  def tag_names
+    tag_list.join(',')
+  end
 end
