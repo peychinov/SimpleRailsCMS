9793195793a9568fc43f70518c5219b85e7fdb57
Dimitar Peychinov <dp86jd@gmail.com>
2013-06-16 20:24:21 +0300

    Removed old code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :set_locale # :load_root_categories
+  before_filter :set_locale
 
   private
 
@@ -9,10 +9,6 @@ class ApplicationController < ActionController::Base
 	  def after_sign_in_path_for(resource_or_scope)
 	    admin_categories_path
 	  end
-
-    # def load_root_categories
-    #   @root_categories = Category.where(:parent_id => nil)
-    # end
 
     def set_locale
       I18n.locale = params[:locale] if params[:locale].present?
